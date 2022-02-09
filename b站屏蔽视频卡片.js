f364c40674e7880d8a0d9ed1b08deecf3683066d
MirionQs <51433180+MirionQs@users.noreply.github.com>
2022-02-09 18:14:57 +0800

    Update b站屏蔽视频卡片.js

--- b站屏蔽视频卡片.js
+++ b站屏蔽视频卡片.js
@@ -1,39 +1,92 @@
 // ==UserScript==
 // @name         b站屏蔽视频卡片
-// @version      2022.1.17
+// @version      2022.2.9
 // @author       Mirion
 // @match        https://www.bilibili.com/*
 // ==/UserScript==
 
 (function () {
 	'use strict';
-	
-	setTimeout(() => {
 
-		var keywordList = [];
+	window.onload = () => {
 
-		var url = window.location.href;
-		var titles, videocards;
-		if (url == 'https://www.bilibili.com/') {
-			titles = document.querySelectorAll('.recommend-container__2-line .bili-video-card__info--tit');
-			videocards = [].slice.apply(document.querySelectorAll('.recommend-container__2-line > .bili-video-card'));
-		} else if (url.startsWith('https://www.bilibili.com/video/')) {
-			titles = document.querySelectorAll('#reco_list .title');
-			videocards = document.querySelectorAll('#reco_list .video-page-card-small');
+		// 用户设置
+		var titleKeywordList = [
+			'原神', '幻塔', '胡桃', '钟离', '米哈游', '雄狮少年', // 晦气
+			'王者荣耀', '崩三', '第五人格', '英雄联盟', '手游', // 手游
+			'学霸', '学渣', '高三', '高中', '高考', '涨分', '提分', '自习', '学姐', '中考', '刷题', // 应试
+			'身材', '健身', '脂肪', '减肥', '瘦身', '减脂', // 健身
+			'拖延', '戒断', '矫正', '自律', // 健康生活
+			'亲身经历', '我做的', '我开发', '自制', // “亲自”
+			'小姐姐', '网文', // 低俗
+			'小米', '手机', // 手机
+			'大厂面试', '元宇宙' // 其它
+		] // 标题关键词屏蔽列表
+		var upList = [] // up主屏蔽列表
+
+		// 辅助函数
+		function _(selector, parent = document) {
+			return parent.querySelector(selector)
 		}
 
-		var count = 0;
-		for (let i of titles) {
-			for (let j of keywordList) {
-				if (i.title.indexOf(j) != -1) {
-					console.log('移除视频卡片    [标题]：%s    [关键词]：%s', i.title, j);
-					videocards[count].remove();
-					break;
+		function _a(selector, parent = document) {
+			return parent.querySelectorAll(selector)
+		}
+
+		// 判断页面类型
+		var pageType
+		var url = document.location.href
+		if (url == 'https://www.bilibili.com/') {
+			pageType = 'main'
+		} else if (url.startsWith('https://www.bilibili.com/video/')) {
+			pageType = 'video'
+		} else {
+			return
+		}
+		console.log(`[屏蔽视频卡片] 当前页面类型: ${pageType}`)
+
+		// 主页逻辑
+		if (pageType == 'main') {
+			let videoList = _a('.recommend-container__2-line > .bili-video-card')
+			for (let i of videoList) {
+				let title = _('.bili-video-card__info--tit', i).innerText.replace(' ', '')
+				for (let j of titleKeywordList) {
+					if (title.indexOf(j) != -1) {
+						i.style = 'display:none'
+						console.log(`[屏蔽视频卡片] 屏蔽一个视频卡片，触发标题关键词：${j}`)
+					}
+				}
+				let up = _('.bili-video-card__info--author', i).innerText.replace(' ', '')
+				for (let j of upList) {
+					if (up == j) {
+						i.style = 'display:none'
+						console.log(`[屏蔽视频卡片] 屏蔽一个视频卡片，来自up：${j}`)
+					}
 				}
 			}
-			count++;
 		}
 
-	}, 2000);
+		// 视频页逻辑
+		if (pageType == 'video') {
+			let videoList = _a('.rec-list > .video-page-card-small')
+			for (let i of videoList) {
+				let title = _('.info .title', i).innerText.replace(' ', '')
+				for (let j of titleKeywordList) {
+					if (title.indexOf(j) != -1) {
+						i.style = 'display:none'
+						console.log(`[屏蔽视频卡片] 屏蔽一个视频卡片，触发标题关键词：${j}`)
+					}
+				}
+				let up = _('.upname .name', i).innerText.replace(' ', '')
+				for (let j of upList) {
+					if (up == j) {
+						i.style = 'display:none'
+						console.log(`[屏蔽视频卡片] 屏蔽一个视频卡片，来自up：${j}`)
+					}
+				}
+			}
+		}
+
+	}
 
 })();
